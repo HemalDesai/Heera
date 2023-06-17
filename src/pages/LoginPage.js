a8b0bbd99f3fd2c8b0496b7bdfa7bc9659c1c472
HemalDesai <hemald332@gmail.com>
2023-06-17 13:35:10 +0530

    login page changed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useRouter } from "next/router";
 
-import { initializeApp } from 'firebase/app';
-import { useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { useEffect } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBu2u70wmf3WLG_T9NqEGMi2Yu4jP75fuo",
@@ -12,9 +12,8 @@ const firebaseConfig = {
   storageBucket: "heera-b7f16.appspot.com",
   messagingSenderId: "968834525982",
   appId: "1:968834525982:web:3cf0f9696c28e63910f70f",
-  measurementId: "G-MMQ9WKKHJP"
+  measurementId: "G-MMQ9WKKHJP",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -28,7 +27,7 @@ export default function LoginPage() {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
-        router.push('/HomePage');
+        router.push("/HomePage");
       } else {
         setUser(null);
       }
@@ -42,58 +41,73 @@ export default function LoginPage() {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
       setUser(user);
-      router.push('/HomePage');
+      router.push("/HomePage");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className='boddy' style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      backgroundColor: 'red',
-  }}>
-    <style jsx>{`
-     .boddy{
-         background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-       background-size: 400% 400%;
-       animation: gradient 15s ease infinite;
-       height: 100vh;}
-       @keyframes gradient {
-        0% {
-          background-position: 0% 50%;
-        }
-        50% {
-          background-position: 100% 50%;
-        }
-        100% {
-          background-position: 0% 50%;
-        }
-      }
-
-      `}</style>
-      <button className='button-17' onClick={signInWithGoogle}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <video
+          src="https://i.imgur.com/e0So3IN.mp4"
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+        <div
           style={{
-              fontWeight: 'lighter',
-              fontSize: '20px',
-              border: '1px solid #ccc',
-              padding: '2px 5px',
-              borderRadius: '10px',
-              backgroundColor: 'whitesmoke',
-              boxShadow: '0 5px 5px rgba(0,0,0,0.1)',
-              width: '200px',
-              height: '50px'
+            display: "flex",
+            position: "absolute",
+            top: "0",
+            right: "0",
+            bottom: "0",
+            left: "0",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-      >
-          Sign in with Google
-      </button>
-  </div>
-  
+        >
+          <div style={{ boxShadow: "0 25px 50px -12px rgba(1, 1, 1, 1)" }}>
+            <button
+              style={{
+                display: "flex",
+                padding: "0.75rem",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "0.5rem",
+                cursor: "pointer",
+                outline: "0",
+                color: "whitesmoke",
+                backgroundColor: "",
+                fontWeight: "semi-bold",
+                fontSize: "30px",
+              }}
+              onClick={signInWithGoogle}
+            >
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-
-
+{
+  /* <button className='button-17' onClick={signInWithGoogle}>
+          Sign in with Google
+      </button> */
+}
